79712a2b3c32553ee7113a2cdfd43035102e56bf
Joanna.chen <joanna.chen@ewinsonic.com>
2023-04-06 21:10:40 +0800

    add transfer logic by reqest url and method

--- server.js
+++ server.js
@@ -2,10 +2,23 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=> {
-    res.writeHead(200,{ "Content-Type" : "text/html" })
-    res.write("Hello is in your area !")
-    res.end()
+    console.log("req: ", req.url, req.method)
+    const headers = {
+        "Content-Type" : "text/html"
+    }
+    if(req.url === '/' && req.method === 'GET'){
+        res.writeHead(200, headers)
+        res.write("Hello is in your area/index !")
 
+    }else if(req.url === '/' && req.method === 'DELETE'){
+        res.writeHead(200, headers)
+        res.write("I want to delete something !")
+        
+    }else{
+        res.writeHead(404, headers)
+        res.write("Not Found 404 !")
+    }
+    res.end()
 })
 
 server.listen(8080, '127.0.0.1', () => console.log("Server is in your area !"))
